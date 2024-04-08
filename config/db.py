39a79e5ef3b6914b4e61c6040e3221f164f617d5
kishan-k9 <kk5005575@gmail.com>
2024-04-09 01:40:46 +0530

    first commit

---
+++ config/db.py
@@ -0,0 +1,4 @@
+from pymongo import MongoClient
+# conn = MongoClient("mongodb+srv://user_datas:0xsoOkShXSmbyUHW@cluster0.jph7ywd.mongodb.net/")
+conn = MongoClient("mongodb+srv://users_dataw:pXDTpbCUNQXxl4WE@cluster0.mevjwyc.mongodb.net/")  
+# conn = MongoClient()  
